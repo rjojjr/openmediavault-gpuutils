2bb1bd71d74e17cb2c722aa389bc92a29f80ceb4
Routmoute <lezotre@gmail.com>
2026-02-06 19:55:59 +0100

    Add max value in GPU Memory and GPU Power graphs

--- usr/share/openmediavault/mkrrdgraph/plugins.d/nvidia-gpu.py
+++ usr/share/openmediavault/mkrrdgraph/plugins.d/nvidia-gpu.py
@@ -12,7 +12,7 @@ class Plugin(openmediavault.mkrrdgraph.IPlugin):
             'color_gpu_mem': '#ff3b30',
             'title_gpu_temperature': 'GPU Temperature',
             'color_gpu_temp': '#ff9500',
-            'title_gpu_power': 'GPU Power Draw',
+            'title_gpu_power': 'GPU Power',
             'color_gpu_power': '#4cd964',
         })
 
@@ -44,13 +44,17 @@ class Plugin(openmediavault.mkrrdgraph.IPlugin):
         args.extend(['--start', config['start']])
         args.extend(['--title', '"{title_gpu_memory}{title_by_period}"'.format(**config)])
         args.append('--slope-mode')
-        args.extend(['--vertical-label', 'Bytes'])
-        args.append('DEF:mem={data_dir}/nvidia-gpu-0/gauge-memory_used.rrd:value:AVERAGE'.format(**config))
-        args.append('AREA:mem{color_gpu_mem}:"Memory Used"'.format(**config))
-        args.append('LINE1:mem#000000')
-        args.append('GPRINT:mem:LAST:"Current\\: %1.2lf%s"')
-        args.append('GPRINT:mem:AVERAGE:"Average\\: %1.2lf%s"')
-        args.append('GPRINT:mem:MAX:"Max\\: %1.2lf%s\\c"')
+        args.extend(['--vertical-label', 'MiB'])
+        args.append('DEF:mem_used={data_dir}/nvidia-gpu-0/gauge-memory_used.rrd:value:AVERAGE'.format(**config))
+        args.append('DEF:mem_total={data_dir}/nvidia-gpu-0/gauge-memory_total.rrd:value:AVERAGE'.format(**config))
+        args.append('AREA:mem_total#d3d3d3:"Memory Total"')
+        args.append('AREA:mem_used{color_gpu_mem}:"Memory Used"'.format(**config))
+        args.append('LINE1:mem_total#000000')
+        args.append('LINE1:mem_used#000000')
+        args.append('GPRINT:mem_used:LAST:"Used (Now)\\: %5.0lfMiB"')
+        args.append('GPRINT:mem_total:LAST:"Total\\: %5.0lfMiB\\c"')
+        args.append('GPRINT:mem_used:AVERAGE:"Used (Avg)\\: %5.0lfMiB"')
+        args.append('GPRINT:mem_used:MAX:"Max\\: %5.0lfMiB\\c"')
         args.append('COMMENT:"\\c"')
         args.append('COMMENT:"{last_update}"'.format(**config))
         openmediavault.mkrrdgraph.call_rrdtool_graph(args)
@@ -85,10 +89,14 @@ class Plugin(openmediavault.mkrrdgraph.IPlugin):
         args.append('--slope-mode')
         args.extend(['--vertical-label', 'Watts'])
         args.append('DEF:power={data_dir}/nvidia-gpu-0/gauge-power.rrd:value:AVERAGE'.format(**config))
-        args.append('AREA:power{color_gpu_power}:"Power Draw"'.format(**config))
+        args.append('DEF:power_limit={data_dir}/nvidia-gpu-0/gauge-power_limit.rrd:value:AVERAGE'.format(**config))
+        args.append('AREA:power_limit#d3d3d3:"Limit"')
+        args.append('AREA:power{color_gpu_power}:"Power"'.format(**config))
+        args.append('LINE1:power_limit#000000')
         args.append('LINE1:power#000000')
-        args.append('GPRINT:power:LAST:"Current\\: %3.1lfW"')
-        args.append('GPRINT:power:AVERAGE:"Average\\: %3.1lfW"')
+        args.append('GPRINT:power:LAST:"Now\\: %3.1lfW"')
+        args.append('GPRINT:power_limit:LAST:"Limit\\: %3.1lfW\\c"')
+        args.append('GPRINT:power:AVERAGE:"Avg\\: %3.1lfW"')
         args.append('GPRINT:power:MAX:"Max\\: %3.1lfW\\c"')
         args.append('COMMENT:"\\c"')
         args.append('COMMENT:"{last_update}"'.format(**config))
